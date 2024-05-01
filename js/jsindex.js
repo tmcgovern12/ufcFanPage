950f66c99777361f32bceb828fac8454a7a969a9
tadhg <tmcgovern@mail.endicott.edu>
2024-05-01 17:30:40 -0400

    Update jsindex.js

--- js/jsindex.js
+++ js/jsindex.js
@@ -1,6 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let slideIndex = 0;
+    showSlides(slideIndex);
+
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+
+    prevButton.addEventListener('click', function() { moveSlide(-1); });
+    nextButton.addEventListener('click', function() { moveSlide(1); });
+
+    function moveSlide(n) {
+        slideIndex += n;
+        showSlides(slideIndex);
+    }
+
+    function showSlides(n) {
+        let slides = document.querySelectorAll(".carousel-images img");
+        if (n >= slides.length) slideIndex = 0;
+        if (n < 0) slideIndex = slides.length - 1;
+
+        slides.forEach(slide => slide.style.display = 'none');
+        slides[slideIndex].style.display = 'block';
+    }
+});
 
 window.onload = function() {
     var currentYear = new Date().getFullYear();
     var copyrightText = "&copy; " + currentYear + " McGovern-UFC";
     document.getElementById("copyright").innerHTML = copyrightText;
+
 };
