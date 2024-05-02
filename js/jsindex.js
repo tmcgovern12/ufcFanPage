6fdc4c752a83c936dd18f32a20a8b45837473a8b
tadhg <tmcgovern@mail.endicott.edu>
2024-05-02 17:41:03 -0400

    -

--- js/jsindex.js
+++ js/jsindex.js
@@ -29,3 +29,33 @@ window.onload = function() {
     document.getElementById("copyright").innerHTML = copyrightText;
 
 };
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        var button = document.getElementById("scroll-top-button");
+        var scrollHeight = document.documentElement.scrollHeight;
+        var clientHeight = document.documentElement.clientHeight;
+        var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+        
+        var thresholdPercentage = 10;
+
+        if ((scrollHeight - scrollPosition - clientHeight) / scrollHeight <= thresholdPercentage / 100) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    }
+
+    document.getElementById("scroll-top-button").onclick = function() {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+    };
+});
+
