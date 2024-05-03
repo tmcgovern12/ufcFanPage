910495ec269c92efc866d0a75d01c1cece78ad6e
tadhg <tmcgovern@mail.endicott.edu>
2024-05-03 16:53:52 -0400

    -

--- js/events.js
+++ js/events.js
@@ -7,36 +7,48 @@ window.onload = function() {
 
 
 
+// events.js
 
 
-function calculateFutureDate() {
+function updateCountdown() {
     
     var currentDate = new Date();
+    var futureDate = new Date(currentDate.getTime() + (5 * 24 * 60 * 60 * 1000)); // Adding 5 days in milliseconds
+
+    
+    var timeDifference = futureDate.getTime() - currentDate.getTime();
+
+    
+    if (timeDifference < 0) {
+        document.getElementById('timer').textContent = "Event expired";
+        return; 
+    }
 
   
-    var futureDate = new Date();
-    futureDate.setDate(currentDate.getDate() + 5);
+    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-   
-    var formattedDate = futureDate.toISOString().split('T')[0];
+    
+    hours = ('0' + hours).slice(-2);
+    minutes = ('0' + minutes).slice(-2);
+    seconds = ('0' + seconds).slice(-2);
 
-    return formattedDate;
+    
+    document.getElementById('timer').textContent = "Event in " + days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
+
+    
+    setTimeout(updateCountdown, 1000);
 }
 
 
-function displayFutureDate(elementId) {
-    
-    var element = document.getElementById(elementId);
-
-    
-    var futureDate = calculateFutureDate();
-
-   
-    element.textContent = "Event in 5 days: " + futureDate;
-}
+updateCountdown();
 
 
-displayFutureDate('timer');
+
+
+
 
 
 
